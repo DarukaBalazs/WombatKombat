983558982772d8dfd1d23018529e6a9c0a025868
DarukaBalazs <daruka.balazs01@gmail.com>
2025-10-10 13:24:17 +0200

    jumo
    You can jump and move

--- Assets/Scripts/Player/Global/PlayerMovement.cs
+++ Assets/Scripts/Player/Global/PlayerMovement.cs
@@ -3,15 +3,40 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement Parameters")]
-    [SerializeField] private Rigidbody2D _rb;
-    [SerializeField] private float _speed = 10;
-    [SerializeField] private float _jumpForce = 15;
+    [Header("Player Component References")]
+    [SerializeField] private Rigidbody2D rb;
 
-    
+    [Header("Player settings")]
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float jumpForce = 15;
 
-    private void Movement()
+    [Header("Grounding")]
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+
+    private float horizontal;
+
+    private void FixedUpdate()
     {
-        
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocityY);
     }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
+ 
 }
