5b1500ad43ac5ddd2063f9ce46e019c190ee3026
DarukaBalazs <daruka.balazs01@gmail.com>
2025-10-20 23:20:07 +0200

    input handle with events

--- Assets/Scripts/Managers/Player/PlayerController.cs
+++ Assets/Scripts/Managers/Player/PlayerController.cs
@@ -28,20 +28,11 @@ public class PlayerController : MonoBehaviour
     public Animator Animator => animator;
     public CharacterData Data => data;
 
-    private void Update()
+    private void Awake()
     {
-        movement.HandleMove(input.MoveInput.x);
-
-        if (input.JumpPressed)
-        {
-            movement.HandleJump();
-        }
-
-        if (input.JumpReleased)
-        {
-            movement.CancelJump();
-        }
-
+        input.OnMove += movement.HandleMove;
+        input.OnJumpPressed += movement.HandleJump;
+        input.OnJumpReleased += movement.CancelJump;
     }
 
     /// <summary>
