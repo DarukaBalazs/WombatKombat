73ce8f3d8188a579c80f8ebb0e7ae76350d501c1
DarukaBalazs <daruka.balazs01@gmail.com>
2025-11-03 12:15:14 +0100

    restructurise

--- Assets/Scripts/Core/Movement/BaseMovement.cs
+++ Assets/Scripts/Core/Movement/BaseMovement.cs
@@ -9,6 +9,7 @@ public class BaseMovement : MonoBehaviour
 {
     [Header("Player settings")]
     [SerializeField] PlayerController controller;
+    [SerializeField] PlayerStateManager state;
     [SerializeField] float speed = 5f;
     [SerializeField] float jumpForce = 5f;
     [SerializeField] float doubleJumpMultiplier = 0.7f;
