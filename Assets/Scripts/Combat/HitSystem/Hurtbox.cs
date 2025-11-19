0b3315ed013e304ba1884815eb08e4779bedfc9d
DarukaBalazs <daruka.balazs01@gmail.com>
2025-11-19 09:49:15 +0100

    cuc

--- Assets/Scripts/Combat/HitSystem/Hurtbox.cs
+++ Assets/Scripts/Combat/HitSystem/Hurtbox.cs
@@ -6,6 +6,7 @@ public class Hurtbox : MonoBehaviour
     [SerializeField] Collider2D col;
     [SerializeField] StockSystem stock;
     [SerializeField] KnockbackHandler kb;
+    [SerializeField] PlayerStateManager state;
 
     private void Awake()
     {
@@ -19,6 +20,15 @@ public class Hurtbox : MonoBehaviour
         bool crit = stock ? stock.RegisterHitAndRollCrit(percentgain) : false;
         if (!kb) kb = GetComponent<KnockbackHandler>();
         Vector2 dir = (transform.position - info.attacker.transform.position).normalized;
+
+        float finalKnockback = knockbackForce;
+        float finalHitstun = hitstun;
+
+        if (state != null && state.HasSuperArmor)
+        {
+            finalKnockback *= 0.5f;
+            finalHitstun *= 0.5f;
+        }
         kb.ApplyKnockback(dir, knockbackForce,hitstun);
 
     }
--- Assets/Scripts/Combat/Steps/SuperArmorStep.cs
+++ Assets/Scripts/Combat/Steps/SuperArmorStep.cs
@@ -5,6 +5,16 @@ namespace Combat
     [CreateAssetMenu(menuName = "Combat/Steps/Super Armor")]
     public class SuperArmorStep : AttackStep
     {
+        public override void OnEnter(AttackRunner ctx)
+        {
+            if (ctx.State != null)
+                ctx.State.EnableSuperArmor();
+        }
 
+        public override void OnExit(AttackRunner ctx)
+        {
+            if (ctx.State != null)
+                ctx.State.DisableSuperArmor();
+        }
     }
 }
