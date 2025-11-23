bc58043a6f23b2ff71411e4d9c6bfb49997e92be
ABAJNOK01 <polgar.mor@gmail.com>
2025-11-23 12:09:22 +0100

    most már csak össze kell drotozni

---
+++ Assets/Scripts/Managers/Manu/SelectionManu/CharacterSelectManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public enum Wombat 
+{
+    Dagi,
+    Base,
+    Mary
+}
+public class CharacterSelectManager : MonoBehaviour
+{
+    private Wombat wombat1 = Wombat.Base;
+    private Wombat wombat2 = Wombat.Base;
+    public CharacterData Base;
+    public CharacterData Mary;
+    public CharacterData Dagi;
+    public CharacterSelectorú Selector;
+
+    public void pluss1()
+    {
+        wombat1++;
+    }
+
+    public void minus1()
+    {
+        wombat1--;
+    }
+
+    public void pluss2()
+    {
+        wombat2++;
+    }
+
+    public void minus2()
+    {
+        wombat2--;
+    }
+    private CharacterData Selection(Wombat wombat)
+    {
+        switch (wombat)
+        {
+            case Wombat.Base:
+                return Base;
+                
+            case Wombat.Dagi:
+                return  Dagi;
+                
+            default:
+                return Mary;
+                
+
+        }
+
+    }
+
+    public void StarWombat()
+    {
+        Selector.player1 = Selection(wombat1);
+        Selector.player2 = Selection(wombat2);
+    }
+}
--- Assets/Sprites/Manu/Select/CharEnum.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public enum CharEnum
-{
-    Base,
-    Dagi,
-    Mary
-}
