c43c385b8d02a209da16b4fcf05d1edaed48c2b6
ABAJNOK01 <polgar.mor@gmail.com>
2026-01-01 18:18:52 +0100

    indikátor 1 kész

--- Assets/Scripts/Managers/Ui/IndikatorManager.cs
+++ Assets/Scripts/Managers/Ui/IndikatorManager.cs
@@ -4,10 +4,44 @@ using UnityEngine.UI;
 
 public class indikator1 : MonoBehaviour
 {
-    public Sprite HPBAR;
+    public Image HPBAR;
     public TextMeshProUGUI ReSpawCounter;
     public TextMeshProUGUI Name;
     public Sprite CharacterArt;
+    private GameObject p1Obj;
+    private StockSystem StockSis;
+    public CharacterSpawnManager SpawnManager;
     
+    
+    public Sprite Marry;
+    public Sprite Dagi;
+    public Sprite Sima;
+
+    private void Start()
+    {
+        p1Obj=SpawnManager.p1Obj;
+        Name.text = CharacterSelectionManager.Instance.player1.characterName;
+
+        StockSis= p1Obj.GetComponent<StockSystem>();
+        ReSpawCounter.text = StockSis.Stocks.ToString();
+        HPBAR.color = new Color((1000 - StockSis.DamagePercent) / 1000, 0, 0);
+
+        switch (Name.text) 
+        {
+            case "Mary":
+                {
+                    CharacterArt = Marry; break;
+                }
+            case "Fat":
+                { CharacterArt = Dagi; break;}
+                default: { CharacterArt = Sima; break; }
+        }
+    }
+
+    private void Update()
+    {
+        ReSpawCounter.text = StockSis.Stocks.ToString();
+        HPBAR.color = new Color((1000 - StockSis.DamagePercent) / 1000, 0, 0);
+    }
 
 }
